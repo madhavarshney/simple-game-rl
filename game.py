b57053b58d70c6868a35cc91d822c8346a477c78
david tso <davidtso1219@gmail.com>
2021-09-18 23:05:25 +0800

    Finish obstacles

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import pygame
 
+from random import randint
+
 WIDTH = 600
 HEIGHT = 900
 PADDING = 8
@@ -16,6 +18,13 @@ def player():
     y = 8 * HEIGHT / 9
     return [x, y]
 
+def obstacle(factor=1):
+    x = randint(0, WIDTH)
+    y = 0
+    size = randint(10, 20)
+    speed = randint(50, 80) / 10 * factor
+    return [x, y, size, speed]
+
 class Game:
 
     def __init__(self, human=True):
@@ -24,6 +33,7 @@ class Game:
             "obstacles": [],
             'over': False
         }
+        self.add_obstacle(10)
 
     def start(self, display=True):
         clock = pygame.time.Clock()
@@ -35,22 +45,32 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.gamestate['over'] = True
 
-            keys_pressed = pygame.key.get_pressed()
-            if keys_pressed[pygame.K_LEFT]:
-                self.go_left()
-
-            elif keys_pressed[pygame.K_RIGHT]:
-                self.go_right()
-
+            self.update()
             if display:
                 self.display()
 
             clock.tick(FPS)
 
+    def update(self):
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_LEFT]:
+            self.go_left()
+
+        elif keys_pressed[pygame.K_RIGHT]:
+            self.go_right()
+
+        for obstacle in self.gamestate['obstacles']:
+            obstacle[1] += obstacle[3]
+            if obstacle[1] > HEIGHT:
+                self.replace_obstacle(obstacle)
+
     def display(self):
         if self.screen:
             self.screen.fill(WHITE)
             pygame.draw.circle(self.screen, BLUE, tuple(self.gamestate['player']), PADDING*2)
+
+            for obstacle in self.gamestate['obstacles']:
+                pygame.draw.circle(self.screen, GREY, tuple(obstacle[:2]), obstacle[2])
             pygame.display.update()
 
     def go_left(self):
@@ -58,6 +78,14 @@ class Game:
 
     def go_right(self):
         self.gamestate['player'][0] = min(WIDTH - PADDING, self.gamestate['player'][0] + SPEED)
+
+    def add_obstacle(self, num):
+        for _ in range(num):
+            self.gamestate['obstacles'].append(obstacle())
+
+    def replace_obstacle(self, old_obstacle):
+        self.add_obstacle(1)
+        self.gamestate['obstacles'].remove(old_obstacle)
 
 
 if __name__ == "__main__":
